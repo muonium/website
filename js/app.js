3188d23e4171e8f30aeca5e24e0c50106fa033e2
hitoshi54 <dylanclement7@gmail.com>
2017-04-25 23:58:12 +0200

    Added "vote" in menus and added title from json language files

--- js/app.js
+++ js/app.js
@@ -1,93 +1,93 @@
 var muiApp = angular.module("muiApp",['ngRoute']);
 
 muiApp.factory('Databinding', function(){
-  var title = 'Muonium : Encrypt your files';
-  var bgImage = 'img/bg.jpg';
-  return {
-    title: function() { return title; },
-    setTitle: function(newTitle) { title = 'Muonium : '+newTitle; },
-    bgImage: function () { return bgImage; },
-    setBgImage : function (img) { bgImage = img; }
-  };
+    var title = 'Muonium : Encrypt your files';
+    var bgImage = 'img/bg.jpg';
+    return {
+        title: function() { return title; },
+        setTitle: function(newTitle) { title = 'Muonium : '+newTitle; },
+        bgImage: function () { return bgImage; },
+        setBgImage : function (img) { bgImage = img; }
+    };
 });
 
 muiApp.controller("muiCtrl", function ($routeParams, $scope, $http, Databinding) {
-  $scope.Databinding = Databinding;
-  if(!window.localStorage.getItem('lang')) {
-    var lg = navigator.language;
-    lg = lg.substr(0,2);
-    window.localStorage.setItem('lang', lg);
-}
-  $scope.lang = window.localStorage.getItem('lang');
-  $http.get('translations/website/'+ $scope.lang +'.json').then(function(response) { $scope.txt = response.data; },function errorCallback(response){window.localStorage.setItem("lang", "en");});
-  $scope.changeLang = function(newLang){
-    window.localStorage.setItem('lang', newLang);
+    $scope.Databinding = Databinding;
+    if(!window.localStorage.getItem('lang')) {
+        var lg = navigator.language;
+        lg = lg.substr(0,2);
+        window.localStorage.setItem('lang', lg);
+    }
     $scope.lang = window.localStorage.getItem('lang');
-    location.reload();
-  };
+    $http.get('translations/website/'+ $scope.lang +'.json').then(function(response) { $scope.txt = response.data; },function errorCallback(response){window.localStorage.setItem("lang", "en");});
+    $scope.changeLang = function(newLang){
+        window.localStorage.setItem('lang', newLang);
+        $scope.lang = window.localStorage.getItem('lang');
+        location.reload();
+    };
 });
 
 muiApp.controller('homeController', function ($routeParams, $scope,$http,Databinding) {
-  Databinding.setTitle('Encrypt your files');
-  Databinding.setBgImage('img/bg.jpg');
+    Databinding.setTitle($scope.txt.global.home);
+    Databinding.setBgImage('img/bg.jpg');
 });
 
 muiApp.controller('aboutController', function ($routeParams, $scope,$http,Databinding) {
-  Databinding.setTitle('About');
-  Databinding.setBgImage('img/bg.jpg');
+    Databinding.setTitle($scope.txt.global.about);
+    Databinding.setBgImage('img/bg.jpg');
 });
 
 muiApp.controller('achieveController', function ($routeParams, $scope,$http,Databinding) {
-  Databinding.setTitle('Achievement');
-  Databinding.setBgImage('img/bg.jpg');
+    Databinding.setTitle($scope.txt.global.adventure);
+    Databinding.setBgImage('img/bg.jpg');
 });
 
 muiApp.controller('donateController', function ($routeParams, $scope,$http,Databinding) {
-  Databinding.setTitle('Donate');
-  Databinding.setBgImage('img/bg.jpg');
+    Databinding.setTitle($scope.txt.global.donate);
+    Databinding.setBgImage('img/bg.jpg');
 });
 
 muiApp.controller('securityController', function ($routeParams, $scope,$http,Databinding) {
-  Databinding.setTitle('Security');
-  Databinding.setBgImage('img/bg.jpg');
+    Databinding.setTitle($scope.txt.global.security);
+    Databinding.setBgImage('img/bg.jpg');
 });
 
 muiApp.controller('helpController', function ($routeParams, $scope,$http,Databinding) {
-  Databinding.setTitle('Help');
-  Databinding.setBgImage('img/bg.jpg');
+    Databinding.setTitle($scope.txt.global.help);
+    Databinding.setBgImage('img/bg.jpg');
 });
 
 muiApp.config(['$locationProvider', '$routeProvider', function config($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
     $routeProvider.
-      when('/',{
+    when('/',{
         templateUrl: function(){
-          return 'pages/home.html';
+            return 'pages/home.html';
         },
         controller: "homeController"
-      }).
-      when('/about',{
+    }).
+    when('/about',{
         templateUrl: "pages/about.html",
         controller: "aboutController"
-      }).
-      when('/achievement',{
+    }).
+    when('/achievement',{
         templateUrl: "pages/achieve.html",
         controller: "achieveController"
-      }).
-      when('/donate',{
+    }).
+    when('/donate',{
         templateUrl: "pages/donate.html",
         controller: "donateController"
-      }).
-      when('/security',{
+    }).
+    when('/security',{
         templateUrl: "pages/security.html",
         controller: "securityController"
-      }).
-      when('/help',{
+    }).
+    when('/help',{
         templateUrl: "pages/help.html",
         controller: "helpController"
-      }).
-      otherwise({redirectTo: '/'});
-  }
+    }).
+    otherwise({redirectTo: '/'});
+}
 ]);
 
 muiApp.directive('menuClose', function() {
